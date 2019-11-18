3e29431d37676ff35e24e70c7d9101435e8932a0
tieuf <ctapha13@gmail.com>
2019-11-18 16:03:08 +0100

    relations

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,2 +1,4 @@
 class Producer < ApplicationRecord
+  has_many :categories
+  
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,4 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+  has_many :subscription_options
 end
