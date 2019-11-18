1182d180d60e2166da77296dcd49cf59c00e3f02
NBn <adressemailcommerciale@gmail.com>
2019-11-18 17:27:31 +0100

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 user1 = User.create!(
   first_name: "Lea",
-  last_name: "Farmer"
-  address: "Les Colverts, Birieux"
+  last_name: "Farmer",
+  address: "Les Colverts, Birieux",
   email: "lea@example.com",
   password: "azerty",
   photo_url: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiytebUjPTlAhXJAmMBHRVYC9sQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.grazia.fr%2Fnews-et-societe%2Fnews%2Ftemoignages-les-fermieres-du-ciel-a-new-york-715052&psig=AOvVaw2KXDGIRvKetwBWDb2iMPE-&ust=1574178063647582"
