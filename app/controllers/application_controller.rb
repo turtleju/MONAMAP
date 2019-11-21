e0446e4b24430f7c7225621c21ad164ce4a82ad8
tieuf <ctapha13@gmail.com>
2019-11-21 18:22:37 +0100

    comment logout path in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
       "/dashboard"
     end
 
-    def after_sign_out_path_for(resource)
-      # return the path based on resource
-    end
+    # def after_sign_out_path_for(resource)
+    #   # return the path based on resource
+    # end
 
 end
