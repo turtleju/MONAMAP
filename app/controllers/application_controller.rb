8099cc377568656f6b2f6ce4f097906869d09742
tieuf <ctapha13@gmail.com>
2019-11-22 18:27:59 +0100

    total_card fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
 
     def after_sign_in_path_for(resource)
       # return the path based on resource
-      "/dashboard"      
+      if current_user.subscription_ids.blank?
+        "/subscriptions/new"
+      else
+        "/dashboard"      
+      end
     end
 
     # def after_sign_out_path_for(resource)
--- app/javascript/plugins/total_card.js
+++ app/javascript/plugins/total_card.js
@@ -1,48 +1,14 @@
 
 const total = () => {
-const total_card = document.querySelector(".total-card");
+    const total_card = document.querySelector(".total-card");
 
-//const inputs = document.querySelectorAll('.subscriptions_target');
-const inputs = document.querySelectorAll('.subscriptions_target label');
-//console.log(inputs);
-//debugger
-
-// inputs.forEach( input =>
-//   input.addEventListener("click", function categories(){
-//     if(input.checked == true ) {
-//         const category = document.querySelector('.script-selector').closest('div .subscription-category-card');
-//         const name = category.querySelector('#category-name').innerText;
-//         // console.log(name);
-//         //total_card.insertAdjacentHTML('afterbegin', '<p>fromages</p>')
-//     }
-//     else {
-//         console.log("je suis ailleurs");
-//     }
-//   }
-//   )
-// );
-    // inputs.forEach(input =>
-    //     input.addEventListener('click', () => {
-    //       const background = "rgb(137, 196, 203)";
-    //       const bgcolor = window.getComputedStyle(input).backgroundColor;
-    //       if (background == bgcolor) {
-    //         const category = input.parentElement.parentNode.parentElement.querySelector('h4').innerText;
-    //         total_card.insertAdjacentHTML('afterbegin',`<p>${category}</p>`);
-    //         console.log(input.htmlFor);
-    //         // "Hello <%= @user.name %>"
-    //         //console.log(bgcolor);
-    //         //console.log(background);
-    //       }
-
-    //     }
-    //     )
-
-    // );
-
+    //const inputs = document.querySelectorAll('.subscriptions_target label');
+    
     document.querySelector('form').addEventListener('change', () => {
-        const inputs = document.querySelectorAll('input[type="radio"]:checked');
-        total_card.innerHTML = '';
         let price = 0;
+        const inputs = document.querySelectorAll('input[type="radio"]:checked');
+        console.log(inputs);
+        total_card.innerHTML = '';
         inputs.forEach((input) => {
           total_card.insertAdjacentHTML('afterbegin',`<p>${input.dataset.category}</p>`);
           price += parseFloat(input.dataset.price);
