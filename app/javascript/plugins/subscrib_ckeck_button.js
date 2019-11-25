f728e0b8d2934e88c72f669aaa1f33c094dfdcef
tieuf <ctapha13@gmail.com>
2019-11-25 10:27:24 +0100

    fix total_card javascript

--- app/javascript/plugins/subscrib_ckeck_button.js
+++ app/javascript/plugins/subscrib_ckeck_button.js
@@ -2,20 +2,17 @@
 const check_box = () => {
 
 const inputs = document.querySelectorAll('.script-selector');
-//const labels = document.querySelectorAll('.subscriptions_target');
-
-
   inputs.forEach(input =>
     input.addEventListener("click", function firstStep(){
       if(input.checked == true){
         input.addEventListener("click", function secondStep() {
           if(input.checked == false){
 
-            input.checked = true
+            input.checked = true;
 
           } else {
-            input.checked = false
-
+            input.checked = false;
+            document.querySelector('.simple_form.category').dispatchEvent(new Event('change'));
           }
         });
       }
