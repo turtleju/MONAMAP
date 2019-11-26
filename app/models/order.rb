33042977d58e6cdd26e51f6817552d191df4fc08
tieuf <ctapha13@gmail.com>
2019-11-26 17:04:26 +0100

    payments view page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :subscription
+  has_many :subscription_options, through: :subscription
+  has_many :options, through: :subscription_options
+
   monetize :amount_cents
 end
