7841841ec0c3dd996f914b27ed89f1e528fb6742
Juliette Guerbette <turtleju@gmail.com>
2019-11-28 11:19:46 +0100

    subNew better and Page Producter OK

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :dashboard, only: :show
   resources :subscriptions, only: [:new, :create]
   resources :payments, only: :new
+  get "producer/:id", to: "producers#show"
 
   mount StripeEvent::Engine, at: '/stripe-webhooks'
   put '/add_user_to_distrib', to: 'dashboards#add_user_to_distrib', as: :distribution
